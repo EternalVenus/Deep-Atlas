9f89f176ea4fc37231940c2f3dad20523cabb283
David Jiang <djiang86@binghamton.edu>
2017-10-12 04:27:05 +0000

    Added Printer Main

---
+++ World/Printer/Main.java
@@ -0,0 +1,10 @@
+public class PrinterMain {
+    public static void main(String[] args){
+        Printer printer = new Printer(50, true);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + "new total print count for printer is " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(10);
+        System.out.println("Pages printed was " + pagesPrinted + "new total print count for printer is " + printer.getPagesPrinted());
+    }
+}
