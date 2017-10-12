eef5879357752c4483fa282b8cc65b9f96032b85
David Jiang <djiang86@binghamton.edu>
2017-10-12 04:27:44 +0000

    Added VipCustomer class

---
+++ World/VIP/VipCustomer.java
@@ -0,0 +1,31 @@
+public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer(){
+        this("Dav", 2000, "CS@gmail.com");
+    }
+
+    public VipCustomer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "hello@gmail.com");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
