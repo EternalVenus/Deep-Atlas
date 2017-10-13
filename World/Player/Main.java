0fc1571067e2721ed7f15662deef63b9e8d1f688
David Jiang <djiang86@binghamton.edu>
2017-10-13 04:29:45 +0000

    Added Player main Class

---
+++ World/Player/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+
+        Player player = new Player("David", 100, "Axe");
+
+        System.out.println("Initial Health is " + player.getHitPoint());
+    }
+}
