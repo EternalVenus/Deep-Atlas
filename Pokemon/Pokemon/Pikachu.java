5ba0b0ff29f3b0ef0f9e72cb2f70d8b74d0dc6d5
David Jiang <djiang86@binghamton.edu>
2017-10-22 23:42:24 +0000

    Added pikachu class

---
+++ Pokemon/Pokemon/Pikachu.java
@@ -0,0 +1,70 @@
+package Skill.Pokemons;
+import Skill.*;
+
+
+public class Pikachu extends Pokemon {
+    private String nickName;
+    private Skill skill1 = new BoltStrike();
+    private Skill skill2 = new Catastropika();
+    private Skill skill3 = new FusionBolt();
+    private Skill skill4 = new TailWhip();
+    private Type type;
+
+
+    public Pikachu(String nickName) {
+        super("Pikachu");
+        this.nickName = nickName;
+        this.type = new Type("Electric");
+    }
+
+    public Pikachu() {
+        super("Pikachu");
+        this.nickName = null;
+        this.type = new Type("Electric");
+    }
+
+    private Pokemon attack(Pokemon enemy, Skill skill){
+        System.out.println(super.getName() + " uses " + skill.getName() + " on " + enemy.getName());
+        int damage = super.getAtk() + skill.getBaseDamage() - enemy.getDefense();
+        enemy.damageTaken(damage);
+        return enemy;
+    }
+
+    public String getNickName() {
+        if (this.nickName != null){
+            return nickName;
+        }else{
+            return super.getName();
+        }
+
+    }
+
+
+    public void showSkills(){
+        System.out.println("1 :" + skill1.getName() + "\n" +
+                            "2 :" + skill2.getName() + "\n" +
+                            "3 :" + skill3.getName() + "\n" +
+                            "4 :" + skill4.getName());
+    }
+
+
+    public Skill getSkill1() {
+        return skill1;
+    }
+
+    public Skill getSkill2() {
+        return skill2;
+    }
+
+    public Skill getSkill3() {
+        return skill3;
+    }
+
+    public Skill getSkill4() {
+        return skill4;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
