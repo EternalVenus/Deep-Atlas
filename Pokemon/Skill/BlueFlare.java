540c086f226ce62df3cf238fcfe3bcb9f88aa6bd
David Jiang <djiang86@binghamton.edu>
2017-10-24 22:40:53 +0000

    added BlueFlare class

---
+++ Pokemon/Skill/BlueFlare.java
@@ -0,0 +1,56 @@
+package Skill;
+
+import Skill.Pokemons.Type;
+
+public class BlueFlare extends Skill{
+    private static final String name = "Blue Flare";
+    private static final Type type = new Type("Fire");
+    private static final String ID = "none";
+    private static final int baseDamage = 130;
+    private static final String description = "Blue Flare deals damage and has a 20% chance of burning the target.\n" +
+            "Fire type Pokémon, those with the ability Water Veil or those behind a Substitute cannot be burned.";
+
+    public BlueFlare() {
+        super(this.ID, this.name, this.type, this.baseDamage, this.description);
+    }
+
+    @Override
+    public int effect(Pokemon pokemon) {
+        return super.effect(pokemon);
+    }
+
+    @Override
+    public int defenseEffect(Pokemon pokemon) {
+        return super.defenseEffect(pokemon);
+    }
+
+    @Override
+    public int speedEffect(Pokemon pokemon) {
+        return super.speedEffect(pokemon);
+    }
+
+    @Override
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Type getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
