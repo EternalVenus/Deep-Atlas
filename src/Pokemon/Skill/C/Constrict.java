d8b5c935d12ed218041763b1f9f026cf51f19688
David Jiang <djiang86@binghamton.edu>
2017-10-28 03:00:01 +0000

    added constrict class

---
+++ src/Pokemon/Skill/C/Constrict.java
@@ -0,0 +1,57 @@
+package Skill.C;
+
+import Pokemons.Type;
+import Skill.*;
+
+public class Constrict extends Skill{
+    private static final String name = "Constrict";
+    private static final Type type = new Type("Normal");
+    private static final String ID = "none";
+    private static final int baseDamage = 10;
+    private static final String description = "Constrict deals damage and has a 10% chance of lowering the target's Speed by one stage.\n" +
+            "Stats can be lowered to a minimum of -6 stages each.";
+
+    public Constrict() {
+        super(this.ID, this.name, this.type, this.baseDamage, this.description);
+    }
+
+    @Override
+    public int effect(Pokemon pokemon) {
+        return super.effect(pokemon);
+    }
+
+    @Override
+    public int defenseEffect(Pokemon pokemon) {
+        return super.defenseEffect(pokemon);
+    }
+
+    @Override
+    public int speedEffect(Pokemon pokemon) {
+        return super.speedEffect(pokemon);
+    }
+
+    @Override
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Type getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
