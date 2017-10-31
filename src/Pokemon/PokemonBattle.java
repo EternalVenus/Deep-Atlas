70bc62f894c518942a8b2096911c6f2a20b20de0
David Jiang <djiang86@binghamton.edu>
2017-10-31 12:37:43 +0000

    updated pokemonbattle class

--- src/Pokemon/PokemonBattle.java
+++ src/Pokemon/PokemonBattle.java
@@ -1,9 +1,40 @@
 package Pokemon;
 
 public class PokemonBattle {
-    PokemonBag bag;
-    PokemonBag bag2;
+    private PokemonPlayer player1;
+    private PokemonPlayer player2;
+    private boolean PlayerTurn;
+    private boolean battleOver;
+    public boolean playerBattle;
 
-    PokemonPlayer player1;
-    PokemonPlayer player2;
+    public PokemonBattle(PokemonPlayer player1, PokemonPlayer player2, boolean playerBattle){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.PlayerTurn = true;
+        this.battleOver = false;
+        this.playerBattle = playerBattle;
+    }
+
+    public void startBattle(){
+        System.out.println("=========BATTLE STARTS=========");
+        if (playerBattle) {
+            System.out.println(player2.getName() + " wants to battle!");
+            System.out.println(player1.getName() + " sent out ");
+//            System.out.println();
+//            while (!battleOver){
+//                if (PlayerTurn){
+//                    showOptions();
+//                }
+//
+//            }
+        }
+
+    }
+
+    private void showOptions(){
+        System.out.println("1)FIGHT    2)BAG \n" +
+                           "3)POKEMON  4)RUN");
+    }
+
+
 }
