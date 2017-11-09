fc68f1afffcae154232a1ace413a9611c4c6ede9
David Jiang <djiang86@binghamton.edu>
2017-11-09 02:37:40 +0000

    updated dive class

--- src/Pokemon/Skill/D/Dive.java
+++ src/Pokemon/Skill/D/Dive.java
@@ -1,21 +1,34 @@
-package Skill.D;
+package Pokemon.Skill.D;
 
-import Pokemons.Type;
-import Skill.*;
+import Pokemon.Type;
+import Pokemon.Skill.*;
+import Pokemon.Pokemon;
 
 public class Dive extends Skill{
-    private static final String name = "Dive";
-    private static final Type type = new Type("Water");
-    private static final String ID = "none";
-    private static final int baseDamage = 80;
-    private static final String description = "The user of Dive will hide underwater on the first turn, disappearing from view and becoming invulnerable to most attacks. On the second turn, Dive deals damage.\n" +
-            "While underwater, the Pokemon can only be hit by the moves Surf, Whirlpool or Low Kick, with Surf and Whirlpool dealing twice normal damage.\n" +
-            "Moves from No Guard Pokemon, or any move following an identify move can also hit for regular power.\n" +
-            "The user also avoids end-of-turn damage from Sandstorm and Hail when underwater.";
+    private  String name ;
+    private Type type;
+    private  String ID;
+    private int baseDamage ;
+    private String description ;
+    private int PP;
+    private String category;
 
     public Dive() {
-        super(this.ID, this.name, this.type, this.baseDamage, this.description, 10);
+        super();
+        this.name = "Dive";
+        this.type  = new Type("Water");
+        this.ID = "HM07";
+        this.baseDamage = 80;
+        this.description = "The user of Dive will hide underwater on the first turn, disappearing from view and becoming invulnerable to most attacks.\n" +
+                "On the second turn, Dive deals damage.\n" +
+                "While underwater, the Pokemon can only be hit by the moves Surf, Whirlpool or Low Kick, with Surf and Whirlpool dealing twice normal damage.\n" +
+                "Moves from No Guard Pokemon, or any move following an identify move can also hit for regular power.\n" +
+                "The user also avoids end-of-turn damage from Sandstorm and Hail when underwater.\n" +
+                "If the user is holding a Power Herb, Dive executes in one turn.";
+        this.PP = 10;
+        this.category = "Physical";
     }
+
 
     @Override
     public int effect(Pokemon pokemon) {
@@ -31,6 +44,7 @@ public class Dive extends Skill{
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
     }
+
 
     @Override
     public String getID() {
@@ -56,4 +70,14 @@ public class Dive extends Skill{
     public String getDescription() {
         return this.description;
     }
+
+    @Override
+    public int getPP() {
+        return this.PP;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
 }
