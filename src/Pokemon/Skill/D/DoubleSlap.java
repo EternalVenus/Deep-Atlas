21646e762cba61b5ab7518ed27849cf235917c03
David Jiang <djiang86@binghamton.edu>
2017-11-09 02:38:57 +0000

    updated double slap class

--- src/Pokemon/Skill/D/DoubleSlap.java
+++ src/Pokemon/Skill/D/DoubleSlap.java
@@ -1,24 +1,34 @@
-package Skill.D;
+package Pokemon.Skill.D;
 
-import Pokemons.Type;
-import Skill.*;
+import Pokemon.Type;
+import Pokemon.Skill.*;
+import Pokemon.Pokemon;
 
 public class DoubleSlap extends Skill {
-    private static final String name = "Double Slap";
-    private static final Type type = new Type("Normal");
-    private static final String ID = "none";
-    private static final int baseDamage = 15;
-    private static final String description = "Double Slap hits 2-5 times per turn used. The probability of each interval is shown to the right, with the total power after each hit.\n" +
-            "Each strike of Double Slap is treated like a separate attack:\n" +
-            "Each strike can get a critical hit independently.\n" +
-            "Abilities that activate on contact (Static, Weak Armor, Iron Barbs etc.) activate for each strike.\n" +
-            "If the Sturdy ability, Focus Sash, or Focus Band activates before the final hit, the subsequent hit will still deal damage and therefore cause the opponent to faint.\n" +
-            "If the first hit breaks a Substitute, the second hit will deal damage to the target Pokémon.\n" +
-            "The move Counter only counters the final hit, but Bide counters the complete damage.\n" +
-            "The Skill Link ability will cause Double Slap to always hit the maximum number of times.";
+    private  String name ;
+    private Type type;
+    private  String ID;
+    private int baseDamage ;
+    private String description ;
+    private int PP;
+    private String category;
 
     public DoubleSlap() {
-        super(this.ID, this.name, this.type, this.baseDamage, this.description, 10);
+        super();
+        this.name = "Double Hit";
+        this.type  = new Type("Normal");
+        this.ID = "none";
+        this.baseDamage = 15;
+        this.description = "Double Slap hits 2-5 times per turn used. The probability of each interval is shown to the right, with the total power after each hit.\n" +
+                "Each strike of Double Slap is treated like a separate attack:\n" +
+                "Each strike can get a critical hit independently.\n" +
+                "Abilities that activate on contact (Static, Weak Armor, Iron Barbs etc.) activate for each strike.\n" +
+                "If the Sturdy ability, Focus Sash, or Focus Band activates before the final hit, the subsequent hit will still deal damage and therefore cause the opponent to faint.\n" +
+                "If the first hit breaks a Substitute, the second hit will deal damage to the target Pokémon.\n" +
+                "The move Counter only counters the final hit, but Bide counters the complete damage.\n" +
+                "The Skill Link ability will cause Double Slap to always hit the maximum number of times.\n";
+        this.PP = 10;
+        this.category = "Physical";
     }
 
 
@@ -36,6 +46,7 @@ public class DoubleSlap extends Skill {
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
     }
+
 
     @Override
     public String getID() {
@@ -60,5 +71,14 @@ public class DoubleSlap extends Skill {
     @Override
     public String getDescription() {
         return this.description;
+    }
+
+    @Override
+    public int getPP() {
+        return this.PP;
+    }
+
+    public String getCategory() {
+        return category;
     }
 }
