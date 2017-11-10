ee1988d8eaa40359c2998312dff18dffd3fdd762
David Jiang <djiang86@binghamton.edu>
2017-11-10 20:38:00 +0000

    added draining kiss

---
+++ src/Pokemon/Skill/D/DrainingKiss.java
@@ -0,0 +1,76 @@
+package Pokemon.Skill.D;
+
+import Pokemon.Type;
+import Pokemon.Skill.*;
+import Pokemon.Pokemon;
+
+public class DrainingKiss extends Skill {
+    private  String name ;
+    private Type type;
+    private  String ID;
+    private int baseDamage ;
+    private String description ;
+    private int PP;
+    private String category;
+
+    public DrainingKiss() {
+        super();
+        this.name = "Draining Kiss";
+        this.type  = new Type("Fairy");
+        this.ID = "none";
+        this.baseDamage = 50;
+        this.description = "Draining Kiss deals damage and the user will recover 75% of the HP drained.\n";
+        this.PP = 10;
+        this.category = "Special";
+    }
+
+    @Override
+    public int effect(Pokemon pokemon) {
+        return super.effect(pokemon);
+    }
+
+    @Override
+    public int defenseEffect(Pokemon pokemon) {
+        return super.defenseEffect(pokemon);
+    }
+
+    @Override
+    public int speedEffect(Pokemon pokemon) {
+        return super.speedEffect(pokemon);
+    }
+
+
+    @Override
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Type getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public int getPP() {
+        return this.PP;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
