97abf8947df4d9724204acd45057ea4e671ab1e8
David Jiang <djiang86@binghamton.edu>
2017-11-10 20:37:22 +0000

    updated dragon hammer

---
+++ src/Pokemon/Skill/D/DragonHammer.java
@@ -0,0 +1,76 @@
+package Pokemon.Skill.D;
+
+import Pokemon.Type;
+import Pokemon.Skill.*;
+import Pokemon.Pokemon;
+
+public class DragonHammer extends Skill {
+    private  String name ;
+    private Type type;
+    private  String ID;
+    private int baseDamage ;
+    private String description ;
+    private int PP;
+    private String category;
+
+    public DragonHammer() {
+        super();
+        this.name = "Draco Hammer";
+        this.type  = new Type("Dragon");
+        this.ID = "TM02";
+        this.baseDamage = 90;
+        this.description ="The user uses its body like a hammer to attack the target and inflict damage.\n";
+        this.PP = 15;
+        this.category = "Physical";
+    }
+
+    @Override
+    public int effect(Pokemon pokemon) {
+        return super.effect(pokemon);
+    }
+
+    @Override
+    public int defenseEffect(Pokemon pokemon) {
+        return super.defenseEffect(pokemon);
+    }
+
+    @Override
+    public int speedEffect(Pokemon pokemon) {
+        return super.speedEffect(pokemon);
+    }
+
+
+    @Override
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Type getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public int getPP() {
+        return this.PP;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
