31ebd87f4a3ec970b65d877f39ba1f9a8b59cb83
David Jiang <djiang86@binghamton.edu>
2017-11-11 07:15:37 +0000

    added explosion

---
+++ src/Pokemon/Skill/E/Explosion.java
@@ -0,0 +1,76 @@
+package Pokemon.Skill.E;
+
+import Pokemon.Type;
+import Pokemon.Skill.*;
+import Pokemon.Pokemon;
+
+public class Explosion extends Skill{
+    private  String name ;
+    private  Type type;
+    private  String ID;
+    private int baseDamage ;
+    private String description ;
+    private int PP;
+    private String category;
+
+    public Explosion() {
+        super();
+        this.name = "Explosion";
+        this.type  = new Type("Normal");
+        this.ID = "TM64";
+        this.baseDamage = 250;
+        this.description= "Explosion deals high damage, but causes the user to faint.\n";
+        this.PP = 5;
+        this.category = "Physical";
+    }
+
+    @Override
+    public int effect(Pokemon pokemon) {
+        return super.effect(pokemon);
+    }
+
+    @Override
+    public int defenseEffect(Pokemon pokemon) {
+        return super.defenseEffect(pokemon);
+    }
+
+    @Override
+    public int speedEffect(Pokemon pokemon) {
+        return super.speedEffect(pokemon);
+    }
+
+
+    @Override
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Type getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public int getPP() {
+        return this.PP;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
