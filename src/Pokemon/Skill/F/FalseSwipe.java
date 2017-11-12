af35af377235bdacb508d803983f04f682425a20
David Jiang <djiang86@binghamton.edu>
2017-11-12 22:42:35 +0000

    added false swipe

---
+++ src/Pokemon/Skill/F/FalseSwipe.java
@@ -0,0 +1,76 @@
+package Pokemon.Skill.F;
+
+import Pokemon.Type;
+import Pokemon.Skill.*;
+import Pokemon.Pokemon;
+
+public class FalseSwipe extends Skill{
+    private  String name ;
+    private  Type type;
+    private  String ID;
+    private int baseDamage ;
+    private String description ;
+    private int PP;
+    private String category;
+
+    public FalseSwipe() {
+        super();
+        this.name = "False Swipe";
+        this.type  = new Type("Normal");
+        this.ID = "TM54";
+        this.baseDamage = 40;
+        this.description= "Always leaves opponent with at least 1 HP.\n";
+        this.PP = 40;
+        this.category = "Physical";
+    }
+
+    @Override
+    public int effect(Pokemon pokemon) {
+        return super.effect(pokemon);
+    }
+
+    @Override
+    public int defenseEffect(Pokemon pokemon) {
+        return super.defenseEffect(pokemon);
+    }
+
+    @Override
+    public int speedEffect(Pokemon pokemon) {
+        return super.speedEffect(pokemon);
+    }
+
+
+    @Override
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Type getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public int getPP() {
+        return this.PP;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
