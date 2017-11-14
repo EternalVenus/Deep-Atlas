f8d11264ef274f2f7e9144fa0d105bf00ebb0f34
David Jiang <djiang86@binghamton.edu>
2017-11-14 19:06:55 +0000

    refactored blizzard and blueflare

--- src/Pokemon/Skill/B/Blizzard.java
+++ src/Pokemon/Skill/B/Blizzard.java
@@ -7,28 +7,17 @@ import Pokemon.Pokemon;
 public class Blizzard extends Skill{
 
     public Blizzard() {
-        super("Absorb",
-                new Type("Grass"),
-                "none",
-                20,
-                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
-                        "the user instead loses the HP it would have otherwise gained.\n",
-                25,
-                "Special"
-                );
-                
-        this.name = "Blizzard";
-        this.type = new Type("Ice");
-        this.ID = "TM14";
-        this.baseDamage = 110;
-        this.description = "Blizzard deals damage and has a 10% chance of freezing the target.\n" +
+        super("Blizzard",
+                new Type("Ice"),
+                "TM14",
+                110,
+                 "Blizzard deals damage and has a 10% chance of freezing the target.\n" +
                 "It will hit both opponents in a double battle or adjacent opponents in a triple battle.\n" +
                 "Ice type Pokémon, those with the ability Magma Armor or those behind a Substitute cannot be frozen.\n" +
-                "During a hailstorm, Blizzard has 100% accuracy.";
-        this.PP = 5;
-        this.category = "Special";
+                "During a hailstorm, Blizzard has 100% accuracy.\n",
+                5,
+                "Special"
+                );
     }
 
     @Override
--- src/Pokemon/Skill/B/BlueFlare.java
+++ src/Pokemon/Skill/B/BlueFlare.java
@@ -8,25 +8,15 @@ public class BlueFlare extends Skill{
 
 
     public BlueFlare() {
-        super("Absorb",
-                new Type("Grass"),
+        super("Blue Flare",
+                new Type("Fire"),
                 "none",
-                20,
-                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
-                        "the user instead loses the HP it would have otherwise gained.\n",
-                25,
+                130,
+                "Blue Flare deals damage and has a 20% chance of burning the target.\n" +
+                "Fire type Pokémon, those with the ability Water Veil or those behind a Substitute cannot be burned.\n",
+                5,
                 "Special"
                 );
-        this.name  = "Blue Flare";
-        this.type = new Type("Fire");
-        this.ID = "none";
-        this.baseDamage = 130;
-        this.description = "Blue Flare deals damage and has a 20% chance of burning the target.\n" +
-                "Fire type Pokémon, those with the ability Water Veil or those behind a Substitute cannot be burned.";
-        this.PP = 5;
-        this.category = "Special";
     }
 
      @Override
