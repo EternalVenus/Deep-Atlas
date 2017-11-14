416902b571a992dd845d13fd258782383c7f97f4
David Jiang <djiang86@binghamton.edu>
2017-11-14 15:02:25 +0000

    refactored absorb and accelerock

--- src/Pokemon/Skill/A/Absorb.java
+++ src/Pokemon/Skill/A/Absorb.java
@@ -1,29 +1,23 @@
 package Pokemon.Skill.A;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class Absorb  extends Skill{
-    private String name;
-    private Type type;
-    private String ID;
-    private int baseDamage;
-    private String description;
-    private int PP;
-    private String category;
 
     public Absorb() {
-        super();
-        this.PP = 25;
-        this.name = "Absorb";
-        this.type = new Type("Grass");
-        this.ID = "none";
-        this.baseDamage = 20;
-        this.description = "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                "If used on a IPokemon with the ability Liquid Ooze, the user instead loses the HP it would have otherwise gained.";
-        this.category = "Special";
+        super("Absorb",
+                new Type("Grass"),
+                "none",
+                20,
+                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
+                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
+                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
+                        "the user instead loses the HP it would have otherwise gained.\n",
+                25,
+                "Special"
+                );
     }
 
 
@@ -43,37 +37,4 @@ public class Absorb  extends Skill{
     }
 
 
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
-    }
 }
--- src/Pokemon/Skill/A/Accelerock.java
+++ src/Pokemon/Skill/A/Accelerock.java
@@ -1,28 +1,22 @@
 package Pokemon.Skill.A;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class Accelerock extends Skill{
-    private String name ;
-    private Type type ;
-    private String ID ;
-    private int baseDamage ;
-    private String description;
-    private int PP;
-    private String category;
 
     public Accelerock() {
-        this.name = "Accelerock";
-        this.type = new Type("Rock");
-        this.ID = "none";
-        this.baseDamage = 40;
-        this.description  = "Accelerock deals damage and has high priority.\n" +
-                "Moves with a higher priority always go first, regardless of a Pokémon's speed.\n" +
-                "If two moves with the same priority are used, the faster Pokémon goes first as usual.";
-        this.PP = 20;
-        this.category = "Physical";
+        super("Accelerock",
+                new Type("Rock"),
+                "none",
+                40,
+                "Accelerock deals damage and has high priority.\n" +
+                        "Moves with a higher priority always go first, regardless of a Pokémon's speed.\n" +
+                        "If two moves with the same priority are used, the faster Pokémon goes first as usual.\n",
+                20,
+                "Physical"
+        );
     }
 
     @Override
@@ -40,37 +34,4 @@ public class Accelerock extends Skill{
         return super.speedEffect(pokemon);
     }
 
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
-    }
 }
