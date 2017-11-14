68b76e88759d962eb4997dcdf1934c9b3b4eb87a
David Jiang <djiang86@binghamton.edu>
2017-11-14 15:04:31 +0000

    refactored attackorder and aurasphere

--- src/Pokemon/Skill/A/AttackOrder.java
+++ src/Pokemon/Skill/A/AttackOrder.java
@@ -1,26 +1,20 @@
 package Pokemon.Skill.A;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class AttackOrder  extends Skill{
-    private  String name ;
-    private Type type;
-    private  String ID ;
-    private  int baseDamage ;
-    private  String description ;
-    private int PP;
-    private String category;
 
     public AttackOrder() {
-        this.name = "Attack Order";
-        this.type  = new Type("Bug");
-        this.ID = "none";
-        this.baseDamage = 90;
-        this.description = "Attack Order deals damage and has an increased critical hit ratio (1⁄8 instead of 1⁄16).";
-        this.PP = 15;
-        this.category = "Physical";
+        super("Attack Order",
+                new Type("Bug"),
+                "none",
+                90,
+                "Attack Order deals damage and has an increased critical hit ratio (1⁄8 instead of 1⁄16).\n",
+                15,
+                "Physical"
+        );
     }
 
     @Override
@@ -36,39 +30,5 @@ public class AttackOrder  extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
--- src/Pokemon/Skill/A/AuraSphere.java
+++ src/Pokemon/Skill/A/AuraSphere.java
@@ -1,27 +1,22 @@
 package Pokemon.Skill.A;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class AuraSphere extends Skill{
-    private  String name ;
-    private  Type type ;
-    private  String ID ;
-    private  int baseDamage ;
-    private  String description;
-    private int PP;
-    private String category;
 
     public AuraSphere() {
-        this.name = "Aura Sphere";
-        this.type = new Type("Fighting");
-        this.ID = "none";
-        this.baseDamage = 80;
-        this.description  = "Aura Sphere deals damage and ignores changes to the Accuracy and Evasion stats.\n" +
-                "However, it will not hit Pokémon during the invulnerable stage of Bounce, Dig, Dive, Fly, Shadow Force or Sky Drop.";
-        this.PP = 20;
-        this.category = "Special";
+        super("Aura Sphere",
+                new Type("Fighting"),
+                "none",
+                80,
+                "Aura Sphere deals damage and ignores changes to the Accuracy and Evasion stats.\n" +
+                        "However, it will not hit Pokémon during the invulnerable stage of Bounce,\n" +
+                        "Dig, Dive, Fly, Shadow Force or Sky Drop.\n",
+                20,
+                "Special"
+        );
     }
 
     @Override
@@ -37,39 +32,5 @@ public class AuraSphere extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
