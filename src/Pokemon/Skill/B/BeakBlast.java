d626c6177130bdc18027a182a5f1a44047d7a54c
David Jiang <djiang86@binghamton.edu>
2017-11-14 19:01:17 +0000

    refactored beakblast and belch

--- src/Pokemon/Skill/B/BeakBlast.java
+++ src/Pokemon/Skill/B/BeakBlast.java
@@ -8,29 +8,18 @@ public class BeakBlast extends Skill{
 
 
     public BeakBlast() {
-        super("Absorb",
-                new Type("Grass"),
+        super("Beak Blast",
+                new Type("Flying"),
                 "none",
-                20,
-                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
-                        "the user instead loses the HP it would have otherwise gained.\n",
-                25,
-                "Special"
+                100,
+                 "The user first heats up its beak, and then it attacks the target.\n"+
+                "Making direct contact with the Pokémon while it’s heating up its beak results in a burn.\n",
+                15,
+                "Physical"
                 ); 
- 
-        this.name = "Beak Blast";
-        this.type = new Type("Flying");
-        this.ID = "none";
-        this.baseDamage  = 100;
-        this.description = "The user first heats up its beak, and then it attacks the target.\n"+
-                "Making direct contact with the Pokémon while it’s heating up its beak results in a burn.";
-        this.PP = 15;
-        this.category = "Physical";
     }
 
- @Override
+    @Override
     public int effect(Pokemon pokemon) {
         return super.effect(pokemon);
     }
--- src/Pokemon/Skill/B/Belch.java
+++ src/Pokemon/Skill/B/Belch.java
@@ -6,26 +6,15 @@ import Pokemon.Pokemon;
 
 public class Belch extends Skill{
     public Belch() {
-        super("Absorb",
-                new Type("Grass"),
+        super("Belch",
+                new Type("Poison"),
                 "none",
-                20,
-                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
-                        "the user instead loses the HP it would have otherwise gained.\n",
-                25,
+                120,
+                 "Belch deals damage, but only works if the user has previously consumed a Berry during the battle.\n" +
+                "Manually feeding a Berry from the bag does not count.\n",
+                10,
                 "Special"
                 );
-                
-        this.name = "Belch";
-        this.type = new Type("Poison");
-        this.ID = "none";
-        this.baseDamage = 120;
-        this.description = "Belch deals damage, but only works if the user has previously consumed a Berry during the battle.\n" +
-                "Manually feeding a Berry from the bag does not count.";
-        this.PP = 10;
-        this.category = "Special";
     }
 
      @Override
