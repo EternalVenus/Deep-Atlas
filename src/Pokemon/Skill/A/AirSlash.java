058455203d0890a3ef4a3b54a45b7be8a0fb54b3
David Jiang <djiang86@binghamton.edu>
2017-11-14 15:03:20 +0000

    refactored airslash and anchorshot

--- src/Pokemon/Skill/A/AirSlash.java
+++ src/Pokemon/Skill/A/AirSlash.java
@@ -1,27 +1,21 @@
 package Pokemon.Skill.A;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class AirSlash extends Skill{
-    private  String name ;
-    private   Type type ;
-    private   String ID ;
-    private   int baseDamage ;
-    private   String description ;
-    private int PP;
-    private String category;
 
     public AirSlash() {
-        this.name = "Air Slash";
-        this.type = new Type("Flying");
-        this.ID = "none";
-        this.baseDamage = 75;
-        this.description = "Air Slash deals damage and has a 30% chance of causing the target to flinch (if the target has not yet moved).\n" +
-                "Pokémon with the ability Inner Focus or those behind a Substitute cannot be made to flinch.";
-        this.PP = 20;
-        this.category = "Special";
+        super("Air Slash",
+                new Type("Flying"),
+                "none",
+                75,
+                "Air Slash deals damage and has a 30% chance of causing the target to flinch (if the target has not yet moved).\n" +
+                "Pokémon with the ability Inner Focus or those behind a Substitute cannot be made to flinch.\n",
+                20,
+                "Special"
+        );
     }
 
     @Override
@@ -37,39 +31,5 @@ public class AirSlash extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
--- src/Pokemon/Skill/A/AnchorShot.java
+++ src/Pokemon/Skill/A/AnchorShot.java
@@ -1,26 +1,21 @@
 package Pokemon.Skill.A;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
-public class AnchorShot extends Skill{
-    private   String name ;
-    private   Type type ;
-    private   String ID ;
-    private   int baseDamage ;
-    private   String description ;
-    private int PP;
-    private String category;
+public class AnchorShot extends Skill {
 
     public AnchorShot() {
-        this.name = "Anchor Shot";
-        this.type = new Type("Steel");
-        this.ID = "none";
-        this.baseDamage = 80;
-        this.description = "The user entangles the target with its anchor chain while attacking. The target becomes unable to flee.";
-        this.PP = 20;
-        this.category = "Physical";
+        super("Anchor Shot",
+                new Type("Steel"),
+                "none",
+                80,
+                "The user entangles the target with its anchor chain while attacking.\n" +
+                        "The target becomes unable to flee.\n",
+                20,
+                "Physical"
+        );
     }
 
     @Override
@@ -36,39 +31,5 @@ public class AnchorShot extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
