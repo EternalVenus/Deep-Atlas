2e62f1dbaff8f6fd011bc142d90bcf7e09ed4f52
David Jiang <djiang86@binghamton.edu>
2017-11-14 19:03:24 +0000

    refactored bing and bite

--- src/Pokemon/Skill/B/Bind.java
+++ src/Pokemon/Skill/B/Bind.java
@@ -7,28 +7,20 @@ import Pokemon.Pokemon;
 public class Bind  extends Skill{
 
     public Bind() {
-        super("Absorb",
-                new Type("Grass"),
+        super("Bind",
+                new Type("Normal"),
                 "none",
+                15,
+                "Bind inflicts damage on the first turn then traps the opponent,\n" + 
+                "causing them to lose 1⁄16 of their maximum HP after each turn, for 4-5 turns.\n" +
+                "If the user holds a Grip Claw then it is always 5 turns.\n",
                 20,
-                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
-                        "the user instead loses the HP it would have otherwise gained.\n",
-                25,
-                "Special"
+                "Physical"
                 );
-        this.name = "Bind";
-        this.type  = new Type("Normal");
-        this.ID = "none";
-        this.baseDamage = 15;
-        this.description = "Bind inflicts damage on the first turn then traps the opponent, causing them to lose 1⁄16 of their maximum HP after each turn, for 4-5 turns.\n" +
-                "If the user holds a Grip Claw then it is always 5 turns.";
-        this.PP = 20;
-        this.category = "Physical";
+                
     }
 
- @Override
+     @Override
     public int effect(Pokemon pokemon) {
         return super.effect(pokemon);
     }
--- src/Pokemon/Skill/B/Bite.java
+++ src/Pokemon/Skill/B/Bite.java
@@ -7,25 +7,15 @@ import Pokemon.Pokemon;
 public class Bite extends Skill{
 
     public Bite() {
-        super("Absorb",
-                new Type("Grass"),
+        super("Bite",
+                new Type("Dark"),
                 "none",
-                20,
-                "Absorb deals damage and the user will recover 50% of the HP drained.\n" +
-                        "If the user is holding a Big Root, the move instead recovers 65% of the damage dealt (30% more than normal). \n" +
-                        "If used on a IPokemon with the ability Liquid Ooze,\n" +
-                        "the user instead loses the HP it would have otherwise gained.\n",
+                60,
+                "Bite deals damage and has a 30% chance of causing the target to flinch (if the target has not yet moved).\n" +
+                "Pokémon with the ability Inner Focus or those behind a Substitute cannot be made to flinch.\n",
                 25,
-                "Special"
+                "Physical"
                 );
-        this.name = "Bite";
-        this.type = new Type("Dark");
-        this.ID = "none";
-        this.baseDamage = 60;
-        this.description  = "Bite deals damage and has a 30% chance of causing the target to flinch (if the target has not yet moved).\n" +
-                "Pokémon with the ability Inner Focus or those behind a Substitute cannot be made to flinch.";
-        this.PP = 25;
-        this.category = "Physical";
     }
 
     @Override
