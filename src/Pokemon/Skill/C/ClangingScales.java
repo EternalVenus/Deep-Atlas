fb3abeae49517045cf37c4536eb78a50e0351e3b
David Jiang <djiang86@binghamton.edu>
2017-11-16 00:48:12 +0000

    refactored clangingscale and clearsmog

--- src/Pokemon/Skill/C/ClangingScales.java
+++ src/Pokemon/Skill/C/ClangingScales.java
@@ -1,30 +1,23 @@
 package Pokemon.Skill.C;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class ClangingScales  extends Skill {
-    private   String name ;
-    private   Type type ;
-    private   String ID ;
-    private   int baseDamage ;
-    private   String description ;
-    private String category;
-    private int PP;
+
 
     public ClangingScales() {
-        super();
-        this.name = "Clanging Scales";
-        this.type  = new Type("Dragon");
-        this.ID = "none";
-        this.baseDamage  = 110;
-        this.description  = "Clanging Scales deals damage but lowers the user's Defense by one stage after attacking.\n" +
-                "Stats can be lowered to a minimum of -6 stages each.\n";
-        this.PP = 5;
-        this.category = "Special";
+        super("Clanging Scales",
+                new Type("Dragon"),
+                "none",
+                110,
+                "Clanging Scales deals damage but lowers the user's Defense by one stage after attacking.\n" +
+                        "Stats can be lowered to a minimum of -6 stages each.\n",
+                5,
+                "Special"
+        );
     }
-
 
     @Override
     public int effect(Pokemon pokemon) {
@@ -39,40 +32,5 @@ public class ClangingScales  extends Skill {
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
--- src/Pokemon/Skill/C/ClearSmog.java
+++ src/Pokemon/Skill/C/ClearSmog.java
@@ -1,27 +1,20 @@
 package Pokemon.Skill.C;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class ClearSmog  extends Skill{
-    private   String name ;
-    private   Type type ;
-    private   String ID ;
-    private   int baseDamage ;
-    private   String description ;
-    private String category;
-    private int PP;
 
     public ClearSmog() {
-        super();
-        this.name = "Clear Smog";
-        this.type  = new Type("Poison");
-        this.ID = "none";
-        this.baseDamage  = 50;
-        this.description = "Removes all of the target's stat changes.\n";
-        this.PP = 15;
-        this.category = "Special";
+        super("Clear Smog",
+                new Type("Poison"),
+                "none",
+                50,
+                "Removes all of the target's stat changes.\n",
+                15,
+                "Special"
+        );
     }
 
     @Override
@@ -37,40 +30,5 @@ public class ClearSmog  extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
