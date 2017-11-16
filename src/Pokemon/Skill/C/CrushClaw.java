dc14fad52ef1d81571c3d427aa049191c68dc22f
David Jiang <djiang86@binghamton.edu>
2017-11-16 00:49:43 +0000

    refactored crushclaw and cut

--- src/Pokemon/Skill/C/CrushClaw.java
+++ src/Pokemon/Skill/C/CrushClaw.java
@@ -1,28 +1,21 @@
 package Pokemon.Skill.C;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class CrushClaw extends Skill{
-    private   String name;
-    private   Type type;
-    private   String ID ;
-    private   int baseDamage;
-    private   String description ;
-    private String category;
-    private int PP;
 
     public CrushClaw() {
-        super();
-        this.name = "Crush Claw";
-        this.type  = new Type("Normal");
-        this.ID = "none";
-        this.baseDamage = 75;
-        this.description = "Crush Claw deals damage and has a 50% chance of lowering the target's Defense by one stage.\n" +
-                "Stats can be lowered to a minimum of -6 stages each.\n";
-        this.PP = 10;
-        this.category = "Physical";
+        super("Crush Claw",
+                new Type("Normal"),
+                "none",
+                75,
+                "Crush Claw deals damage and has a 50% chance of lowering the target's Defense by one stage.\n" +
+                        "Stats can be lowered to a minimum of -6 stages each.\n",
+                10,
+                "Physical"
+        );
     }
 
     @Override
@@ -38,40 +31,5 @@ public class CrushClaw extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
--- src/Pokemon/Skill/C/Cut.java
+++ src/Pokemon/Skill/C/Cut.java
@@ -1,27 +1,19 @@
 package Pokemon.Skill.C;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class Cut extends Skill{
-    private   String name ;
-    private   Type type ;
-    private   String ID ;
-    private   int baseDamage;
-    private   String description ;
-    private String category;
-    private int PP;
-
     public Cut() {
-        super();
-        this.name= "Cut";
-        this.type= new Type("Normal");
-        this.ID = "HM01";
-        this.baseDamage = 50;
-        this.description  = "Cut deals damage with no additional effect in battle.\n";
-        this.PP = 30;
-        this.category = "Physical";
+        super("Cut",
+                new Type("Normal"),
+                "HM01",
+                50,
+                "Cut deals damage with no additional effect in battle.\n",
+                30,
+                "Physical"
+        );
     }
 
     @Override
@@ -37,40 +29,5 @@ public class Cut extends Skill{
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
