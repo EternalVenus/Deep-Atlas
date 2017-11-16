3988ee6ed8e82cbca104ac3ae48675aa525e7ecc
David Jiang <djiang86@binghamton.edu>
2017-11-16 00:47:57 +0000

    refactored circlethrow and clamp

--- src/Pokemon/Skill/C/CircleThrow.java
+++ src/Pokemon/Skill/C/CircleThrow.java
@@ -1,27 +1,21 @@
 package Pokemon.Skill.C;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class CircleThrow  extends Skill {
-    private   String name;
-    private   Type type ;
-    private   String ID ;
-    private   int baseDamage ;
-    private   String description ;
-    private String category;
-    private int PP;
+
 
     public CircleThrow() {
-        super();
-        this.name  = "Circle Throw";
-        this.type  = new Type("Fighting");
-        this.ID = "none";
-        this.baseDamage  = 60;
-        this.description = "In battles, the opponent switches. In the wild, the Pokémon runs.\n";
-        this.PP = 10;
-        this.category = "Physical";
+        super("Circle Throw",
+                new Type("Fighting"),
+                "none",
+                60,
+                "In battles, the opponent switches. In the wild, the Pokémon runs.\n",
+                10,
+                "Physical"
+        );
     }
 
 
@@ -38,40 +32,5 @@ public class CircleThrow  extends Skill {
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
--- src/Pokemon/Skill/C/Clamp.java
+++ src/Pokemon/Skill/C/Clamp.java
@@ -1,30 +1,21 @@
 package Pokemon.Skill.C;
 
-import Pokemon.Pokemons.Type;
+import Pokemon.Type;
 import Pokemon.Skill.*;
 import Pokemon.Pokemon;
 
 public class Clamp  extends Skill {
-    private   String name ;
-    private   Type type ;
-    private   String ID;
-    private   int baseDamage ;
-    private   String description;
-    private String category;
-    private int PP;
-
     public Clamp() {
-        super();
-        this.name = "Clamp";
-        this.type  = new Type("Water");
-        this.ID = "none";
-        this.baseDamage = 35;
-        this.description  = "Clamp inflicts damage on the first turn then traps the opponent, causing them to lose 1⁄16 of their maximum HP after each turn, for 4-5 turns.\n" +
-                "If the user holds a Grip Claw then it is always 5 turns.\n";
-        this.PP = 10;
-        this.category = "Physical";
+        super("Clamp",
+                new Type("Water"),
+                "none",
+                35,
+                "Clamp inflicts damage on the first turn then traps the opponent, causing them to lose 1⁄16 of their maximum HP after each turn, for 4-5 turns.\n" +
+                        "If the user holds a Grip Claw then it is always 5 turns.\n",
+                10,
+                "Physical"
+        );
     }
-
 
     @Override
     public int effect(Pokemon pokemon) {
@@ -39,40 +30,5 @@ public class Clamp  extends Skill {
     @Override
     public int speedEffect(Pokemon pokemon) {
         return super.speedEffect(pokemon);
-    }
-
-
-    @Override
-    public String getID() {
-        return this.ID;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Type getType() {
-        return this.type;
-    }
-
-    @Override
-    public int getBaseDamage() {
-        return this.baseDamage;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
-
-    @Override
-    public int getPP() {
-        return this.PP;
-    }
-
-    public String getCategory() {
-        return category;
     }
 }
