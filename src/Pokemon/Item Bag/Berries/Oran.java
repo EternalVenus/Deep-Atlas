bfd0b3ab1295fc0254b713105b23fec5677b18f5
David Jiang <djiang86@binghamton.edu>
2017-11-23 03:10:22 +0000

    added Berry Folder

---
+++ src/Pokemon/Item Bag/Berries/Oran.java
@@ -0,0 +1,16 @@
+package Pokemon.ItemBag.Berries;
+
+import Pokemon.ItemBag.Berry;
+
+public class Oran extends Berry{
+
+    public Oran() {
+        super("Oran Berry",
+                "A Berry to be consumed by Pokémon. If a Pokémon holds one, it can restore its own HP by 10 points during battle.\n",
+                 16,
+                 0,
+                 1.4,
+                 "Can be found in berry piles in Routes 2, 8, 16, and Berry fields.\n",
+                 "Super hard\n");
+    }
+}
