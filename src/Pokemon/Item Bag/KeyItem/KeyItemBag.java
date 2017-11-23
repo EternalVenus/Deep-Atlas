cd4fd186a90ba88ce3b922bc9c1b5bbd7940b161
David Jiang <djiang86@binghamton.edu>
2017-11-23 04:37:57 +0000

    added Key Item Folder

---
+++ src/Pokemon/Item Bag/KeyItem/KeyItemBag.java
@@ -0,0 +1,11 @@
+package Pokemon.ItemBag.KeyItem;
+
+import java.util.ArrayList;
+
+public class KeyItemBag {
+    private ArrayList<KeyItems> keyItemList;
+
+    public KeyItemBag(){
+        this.keyItemList = new ArrayList<>();
+    }
+}
---
+++ src/Pokemon/Item Bag/KeyItem/KeyItems.java
@@ -0,0 +1,29 @@
+package Pokemon.ItemBag.KeyItem;
+
+public class KeyItems{
+    private String name;
+    private String location;
+    private String description;
+
+    public KeyItems(String name, String location, String description) {
+        this.name = name;
+        this.location = location;
+        this.description = description;
+    }
+    public String getClassName(){
+        return "Key Item";
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
