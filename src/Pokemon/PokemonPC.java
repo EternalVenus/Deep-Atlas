df0e454caa42f3ccac5210be3d1de04391829241
David Jiang <djiang86@binghamton.edu>
2017-11-25 15:31:01 +0000

    added emoji to pokemonPC class

--- src/Pokemon/PokemonPC.java
+++ src/Pokemon/PokemonPC.java
@@ -20,14 +20,14 @@ public class PokemonPC {
 
     public boolean storePokemon(Pokemon pokemon, int boxNum){
         if (boxNum > MAX_BOX){
-            System.out.println("There is only " + MAX_BOX + " box in the PC.");
+            System.out.println("There are only " + MAX_BOX + " boxes in the PC (Ò 皿 Ó ╬)");
             return false;
         }else if(boxNum < 0){
-            System.out.println("The number on the box is 0 - 30");
+            System.out.println("Pick a box (0 - 30) (Ò 皿 Ó ╬)");
             return false;
         }else{
             this.box.get(boxNum).addToBox(pokemon);
-            System.out.println(pokemon.getName() + " has successfully added to box " + boxNum);
+            System.out.println(pokemon.getName() + " has successfully added to box " + boxNum + " ヾ(@°▽°@)ノ");
             return true;
         }
     }
@@ -38,14 +38,14 @@ public class PokemonPC {
             if (this.bag.bagSize() < 6){
                 bag.addToBag(pokemon);
                 this.box.get(boxNum).remove(pokemon);
-                System.out.println(pokemon.getName() + " has successfully added to bag.");
+                System.out.println(pokemon.getName() + " has successfully added to bag. ヾ（*⌒ヮ⌒*）ゞ");
                 return this.bag;
             }else{
-                System.out.println("Bag is full. Can not store " + pokemon.getName() + " into the bag.");
+                System.out.println("Bag is full. Can not store " + pokemon.getName() + " into the bag ヘ(￣ー￣ヘ)");
                 return this.bag;
             }
         }else{
-            System.out.println(pokemon.getName() + " is not found in this box.");
+            System.out.println(pokemon.getName() + " is not found in this box (☄ฺ◣д◢)☄");
             return this.bag;
         }
     }
@@ -53,24 +53,24 @@ public class PokemonPC {
     public boolean releasePokemon(Pokemon pokemon, int boxNum){
         if (foundPokemon(pokemon, boxNum)){
             this.box.get(boxNum).remove(pokemon);
-            System.out.println(pokemon.getName() + " has been released");
-            System.out.println("Bye bye " + pokemon.getName());
+            System.out.println(pokemon.getName() + " has been released ʕ ࿃࿆ _ ࿃࿆ ʔ");
+            System.out.println("Bye bye " + pokemon.getName() + " ヽ༼၀-၀༽ﾉ");
             return true;
         }else{
-            System.out.println(pokemon.getName() + " not found.");
+            System.out.println(pokemon.getName() + " not found (ꐦ ಠ皿ಠ )");
             return false;
         }
     }
 
     public void showPokemon(int boxNum){
-        System.out.println("--------------------------------------");
-        System.out.println("Listing IPokemon in box " + boxNum);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("ʕ•̫͡•ʕ*̫͡*ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ*̫͡*ʔ-̫͡-ʔ Listing Pokemon in box " + boxNum + " ʕ•̫͡•ʕ*̫͡*ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ*̫͡*ʔ-̫͡-ʔ");
         if (boxNum >= 0 && boxNum < 31){
             this.box.get(boxNum).showPokemon();
         }else{
-            System.out.println("There is no such box in the PC.");
+            System.out.println("There is no such box in the PC (｡☉︵ ಠ╬)");
         }
-        System.out.println("--------------------------------------");
+        System.out.println("-------------------------------------------------------------------");
     }
 
     private boolean foundPokemon(Pokemon pokemon, int boxNum){
